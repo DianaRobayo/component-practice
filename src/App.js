cfffbbd8c2900d14b0693e12bfd6794c2c2df0c9
DianaRobayo <dmrcupn92@gmail.com>
2024-05-27 22:39:28 -0500

    Change the style y new components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import MyComponent from './components/MyComponent';
+import SecondComponent from './components/SecondComponent';
+import { ThirdComponent } from './components/ThirdComponent';
+import { Child } from './components/Child';
+import { useState } from 'react';
+import { FourthComponent } from './components/FourthComponent';
 
 function App() {
+  const [name, setName] = useState('Lina');
+  const [message, setMessage] = useState('Lina');
+
+  const addMessage = (messages) => {
+    console.log('mes', messages)
+    setMessage(messages);
+  }
+
+
+
+  const medicalRecord = {
+    height: "160",
+    bloodGroup: "A+",
+    allergies: "No"
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +31,28 @@ function App() {
         <p>
           Estructura inicial del proyecto y limpia.
         </p>
+        <div>
+          <FourthComponent />
+          <br /><hr />
+        </div>
+
+        <div>
+          <Child name={name} setName={setName} message={addMessage} />
+          <h1>{message}</h1>
+          <br /><hr />
+        </div>
+
+        <div>
+          <ThirdComponent
+            name="Diana"
+            lastname="Robayo"
+            obj={medicalRecord}
+          />
+          <br /><hr />
+        </div>
+
+        <SecondComponent />
+        <MyComponent />
       </header>
     </div>
   );
